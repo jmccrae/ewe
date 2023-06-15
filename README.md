b2ca2451f2f7c16adf850fae62abe77eb34e2b8b
John P. McCrae <john@mccr.ae>
2023-06-15 10:23:21 +0100

    Finish automaton and write README

--- README.md
+++ README.md
@@ -45,3 +45,79 @@ Building EWE
 EWE can be built with Cargo as follows
 
     cargo build --release
+
+Automating EWE
+--------------
+
+EWE can be automated with an automaton file as follows
+
+    ewe automaton.yaml /path/to/wn
+
+An example of the usage of the automaton file is given below
+
+```yaml
+---
+- entry:
+    synset: 00001740-n
+    lemma: bar
+    action:
+      add:
+        pos: n
+- entry:
+    synset: 00001740-n
+    lemma: bar
+    action: delete
+- entry:
+    synset: 00001740-n
+    lemma: bar
+    action:
+      move:
+        target_synset: 00001741-n
+- synset:
+    action:
+      add:
+        definition: something or someone
+        lexfile: noun.animal
+        pos: n
+        lemmas:
+          - bar
+- synset:
+    action:
+      delete:
+        synset: 00001740-n
+        reason: "Duplicate (#123)"
+        superseded_by: 00001741-n
+- definition:
+    synset: 00001740-n
+    definition: This is a definition
+- example:
+    synset: 00001740-n
+    action:
+      add:
+        example: This is an example
+        source: This is a source
+- example:
+    synset: 00001740-n
+    action:
+      delete:
+        number: 1
+- relation:
+    action:
+      add:
+        source: 00001740-n
+        relation: hypernym
+        target: 00001741-n
+- relation:
+    action:
+      delete:
+        source: 00001740-n
+        source_sense: "example%1:09:00::"
+        target: 00001741-n
+        target_sense: "target%1:10:00::'"
+- relation:
+    action:
+      reverse:
+        source: 00001740-n
+        target: 00001741-n
+- validate
+```
--- src/automaton.rs
+++ src/automaton.rs
@@ -205,12 +205,14 @@ pub enum EntryAction {
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub enum SynsetAction {
+    #[serde(rename = "add")]
     Add {
         definition : String,
         lexfile : String,
         pos : PosKey,
         lemmas : Vec<String>,
     },
+    #[serde(rename = "delete")]
     Delete {
         synset : SynsetId,
         reason : String,
@@ -220,11 +222,14 @@ pub enum SynsetAction {
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub enum ExampleAction {
+    #[serde(rename = "add")]
     Add {
         example : String,
         #[serde(default)]
+        #[serde(skip_serializing_if = "Option::is_none")]
         source : Option<String>
     },
+    #[serde(rename = "delete")]
     Delete {
         number : usize
     }
@@ -232,29 +237,38 @@ pub enum ExampleAction {
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub enum RelationAction {
+    #[serde(rename = "add")]
     Add {
         source : SynsetId,
         #[serde(default)]
+        #[serde(skip_serializing_if = "Option::is_none")]
         source_sense : Option<SenseId>,
         relation : String,
         target : SynsetId,
         #[serde(default)]    
+        #[serde(skip_serializing_if = "Option::is_none")]
         target_sense : Option<SenseId>
     },
+    #[serde(rename = "delete")]
     Delete {
         source : SynsetId,
         #[serde(default)]
+        #[serde(skip_serializing_if = "Option::is_none")]
         source_sense : Option<SenseId>,
         target : SynsetId,
         #[serde(default)]
+        #[serde(skip_serializing_if = "Option::is_none")]
         target_sense : Option<SenseId>
     },
+    #[serde(rename = "reverse")]
     Reverse {
         source : SynsetId,
         #[serde(default)]
+        #[serde(skip_serializing_if = "Option::is_none")]
         source_sense : Option<SenseId>,
         target : SynsetId,
         #[serde(default)]
+        #[serde(skip_serializing_if = "Option::is_none")]
         target_sense : Option<SenseId>
     }
 }
@@ -265,15 +279,86 @@ mod tests {
 
     #[test]
     fn test_serialize() {
-        let test_str = "---\nentry:\n  synset: 00001740-n\n  lemma: bar\n  action:\n    add:\n      pos: n\n";        
-
-        let data = Action::Entry {
-            synset: SynsetId::new("00001740-n"),
-            lemma: "bar".to_string(),
-            action: EntryAction::Add {
-                pos: PosKey::new("n".to_string())
-            }
-        };
+        let test_str = "---\n- entry:\n    synset: 00001740-n\n    lemma: bar\n    action:\n      add:\n        pos: n\n- entry:\n    synset: 00001740-n\n    lemma: bar\n    action: delete\n- entry:\n    synset: 00001740-n\n    lemma: bar\n    action:\n      move:\n        target_synset: 00001741-n\n- synset:\n    action:\n      add:\n        definition: something or someone\n        lexfile: noun.animal\n        pos: n\n        lemmas:\n          - bar\n- synset:\n    action:\n      delete:\n        synset: 00001740-n\n        reason: \"Duplicate (#123)\"\n        superseded_by: 00001741-n\n- definition:\n    synset: 00001740-n\n    definition: This is a definition\n- example:\n    synset: 00001740-n\n    action:\n      add:\n        example: This is an example\n        source: This is a source\n- example:\n    synset: 00001740-n\n    action:\n      delete:\n        number: 1\n- relation:\n    action:\n      add:\n        source: 00001740-n\n        relation: hypernym\n        target: 00001741-n\n- relation:\n    action:\n      delete:\n        source: 00001740-n\n        source_sense: \"example%1:09:00::\"\n        target: 00001741-n\n        target_sense: \"target%1:10:00::'\"\n- relation:\n    action:\n      reverse:\n        source: 00001740-n\n        target: 00001741-n\n- validate\n";
+        let data = vec![Action::Entry {
+                synset: SynsetId::new("00001740-n"),
+                lemma: "bar".to_string(),
+                action: EntryAction::Add {
+                    pos: PosKey::new("n".to_string())
+                }
+            },
+            Action::Entry {
+                synset: SynsetId::new("00001740-n"),
+                lemma: "bar".to_string(),
+                action: EntryAction::Delete
+            },
+            Action::Entry {
+                synset: SynsetId::new("00001740-n"),
+                lemma: "bar".to_string(),
+                action: EntryAction::Move {
+                    target_synset: SynsetId::new("00001741-n")
+                }
+            },
+            Action::Synset {
+                action: SynsetAction::Add {
+                    definition: "something or someone".to_string(),
+                    lexfile: "noun.animal".to_string(),
+                    pos: PosKey::new("n".to_string()),
+                    lemmas: vec!["bar".to_string()]
+                },
+            },
+            Action::Synset {
+                action: SynsetAction::Delete {
+                    synset: SynsetId::new("00001740-n"),
+                    reason: "Duplicate (#123)".to_string(),
+                    superseded_by: SynsetId::new("00001741-n")
+                }
+            },
+            Action::Definition {
+                synset: SynsetId::new("00001740-n"),
+                definition: "This is a definition".to_string()
+            },
+            Action::Example {
+                synset: SynsetId::new("00001740-n"),
+                action: ExampleAction::Add {
+                    example: "This is an example".to_string(),
+                    source: Some("This is a source".to_string())
+                }
+            },
+            Action::Example {
+                synset: SynsetId::new("00001740-n"),
+                action: ExampleAction::Delete {
+                    number: 1
+                }
+            },
+            Action::Relation {
+                action: RelationAction::Add {
+                    source: SynsetId::new("00001740-n"),
+                    source_sense: None,
+                    relation: "hypernym".to_string(),
+                    target: SynsetId::new("00001741-n"),
+                    target_sense: None
+                }
+            },
+            Action::Relation {
+                action: RelationAction::Delete {
+                    source: SynsetId::new("00001740-n"),
+                    source_sense: Some(SenseId::new("example%1:09:00::".to_string())),
+                    target: SynsetId::new("00001741-n"),
+                    target_sense: Some(SenseId::new("target%1:10:00::'".to_string()))
+                }
+            },
+            Action::Relation {
+                action: RelationAction::Reverse {
+                    source: SynsetId::new("00001740-n"),
+                    source_sense: None,
+                    target: SynsetId::new("00001741-n"),
+                    target_sense: None
+                }
+            },
+            Action::Validate
+        ];
+                
         let gen_str : String = serde_yaml::to_string(&data).unwrap();
 
         assert_eq!(test_str, gen_str);
